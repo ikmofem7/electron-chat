6fab69793a840d5af26bed347602ac05af79c0af
ikmofem7 <mohamedikramks7@gmail.com>
2023-09-23 20:26:07 +0530

    added activate inside whenready call

--- main.js
+++ main.js
@@ -16,12 +16,14 @@ function createWindow() {
   win.webContents.openDevTools();
 }
 
-app.whenReady().then(createWindow);
+app.whenReady().then(() => {
+  createWindow();
+  app.on("activate", () => {
+    if (BrowserWindow.getAllWindows().length === 0) {
+      createWindow();
+    }
+  });
+});
 app.on("window-all-closed", () => {
   if (process.platform == "darwin") app.quit();
-});
-app.on("activate", () => {
-  if (BrowserWindow.getAllWindows().length === 0) {
-    createWindow();
-  }
 });
